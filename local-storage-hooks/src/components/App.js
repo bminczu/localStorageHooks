2614ced91b7b9f64f92f0f9d8edbc1e68e3e2427
bminczu <bminczu@gmail.com>
2021-08-26 16:04:07 -0500

    Form2

--- local-storage-hooks/src/components/App.js
+++ local-storage-hooks/src/components/App.js
@@ -1,4 +1,5 @@
 import Form1 from './Form1.js'
+import Form2 from './Form2.js';
 import '../app.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="container">
       <h1>localStorage with React Hooks</h1>
       <Form1/>
-  
+      <Form2/>
     </div>
   );
 }
