4e8d923416b568ed49e60b902031fbd910f49488
bminczu <bminczu@gmail.com>
2021-08-26 14:31:43 -0500

    intiial setup

---
+++ local-storage-hooks/src/components/App.js
@@ -0,0 +1,16 @@
+import Form1 from './Form1.js'
+import './App.css';
+
+function App() {
+  return (
+
+
+    <div className="container">
+      <h1>localStorage with React Hooks</h1>
+      <Form1/>
+  
+    </div>
+  );
+}
+
+export default App;
